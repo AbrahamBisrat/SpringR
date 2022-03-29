d6242bc00c2efc53855da73b0254bde706c071cc
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-29 03:03:36 -0500

    Spring Initializr boiler plate

---
+++ src/test/java/com/SincerePersuit/springWebApp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SincerePersuit.springWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
